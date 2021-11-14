207499aff706ff00de84a83688e5f9d7b060b46d
AlexWanderman <alex.mail949@gmail.com>
2021-11-14 16:44:41 +0300

    Test draft 0.1

--- shop_service/run_test.py
+++ shop_service/run_test.py
@@ -5,7 +5,9 @@ from app import create_app, models, db as _db
 
 @pytest.fixture(scope='session')
 def app(request):
-    return create_app('config.TestingConfig')
+    app = create_app()
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    return app
 
 
 @pytest.fixture(autouse=True)
@@ -44,3 +46,12 @@ def session(request, app, db):
 class Test_0:
     def test_0(self, client, session):
         assert True
+    
+    @pytest.mark.parametrize('login, email, password, first_name, last_name', (
+        ('user_1', 'user_1@mail.com', 'Fname', 'Lname'),
+        ('user_2', 'user_2@mail.com', 'Fname', 'Lname'),
+        ('user_3', 'user_3@mail.com', 'Fname', 'Lname'),
+    ))
+    def test_1(self, client, session, login, email, password, first_name, last_name):
+        session.add(models.UserModel(login, email, password, first_name, last_name))
+        session.commit()
